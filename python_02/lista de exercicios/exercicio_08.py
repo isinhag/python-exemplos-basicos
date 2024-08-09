7bd20c4bda291f2dd4f91f33f771825914d19d4b
isinhag <130663230+isinhag@users.noreply.github.com>
2024-08-09 14:00:54 -0300

    commit

--- python_02/lista de exercicios/exercicio_08.py
+++ python_02/lista de exercicios/exercicio_08.py
@@ -5,7 +5,13 @@ print(" ")
 # Array das lojas 
 nomes = ["Carol", "Esther", "Isabela", "Lívia", "Pedro"]
 
-# Exibindo Lojas
-for i, nomes in enumerate(nomes,1):
-    print(f"{i} = {nomes}")
-    print(" ")
+# # Exibindo Lojas
+# for i, nomes in enumerate(nomes,1):
+#     print(f"{i} = {nomes}")
+#     print(" ")
+
+contador = 1 
+for nome in nomes:
+    print(f"{contador}. {nome}")
+    contador +=1
+
